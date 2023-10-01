1e3db1180b50ae072383a1d13b209b9413de9f02
antorahad <antorahad6@gmail.com>
2023-10-02 00:22:35 +0600

    fire auth

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
       <label tabIndex={0} className="btn btn-ghost btn-circle">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
       </label>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-3 shadow bg-slate-950 text-slate-200 rounded-md w-60 h-[200px] space-y-5">
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-3 shadow bg-slate-950 text-slate-200 rounded-md w-[400px] mx-auto lg:w-60 h-[200px] space-y-5">
         <li><Link to={'/'}>Home</Link></li>
         <li><Link to={'/portfolio'}>Portfolio</Link></li>
         <li><Link to={'/login'}>Login</Link></li>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,22 +1,35 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
 import auth from "./Firebase";
 
 const Register = () => {
+    const [registerError, setRegisterError] = useState('');
+    const [registerSuccess, setRegisterSuccess] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
     const handleRegister = e => {
         e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
         console.log(email, password)
+        setRegisterSuccess('');
+        setRegisterError ('');
+        if(password.length < 6 ){
+            setRegisterError('Password should be at least 6 char or more');
+            return;
+        }else if (!/[A-Z]/.test(password)){
+            setRegisterError('Your Password should have at least one uppercase letter');
+            return;
+        }
         createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             // Signed up 
             const user = userCredential.user;
-            console.log(user);
+            setRegisterSuccess('Account Created Successfully');
             // ...
           })
           .catch((error) => {
             const errorMessage = error.message;
-            console.log(errorMessage);
+            setRegisterError(errorMessage);
             // ..
           });
         
@@ -29,12 +42,25 @@ const Register = () => {
             <br />
             <br />
             <form onSubmit={handleRegister} className='flex items-center justify-center flex-col'>
-                <input className='input input-primary' name='email' type="email" placeholder='Email' />
+                <input className='input input-primary' name='email' type="email" placeholder='Email' required/>
                 <br />
-                <input className='input input-primary' name='password' type="password" placeholder='Password' />
+                <input 
+                className='input input-primary' 
+                name='password' type={ showPassword? "text" : "password"} 
+                placeholder='Password' required />
+                <span onClick={() => setShowPassword(!showPassword)}>Show</span>
                 <br />
                 <button className='btn btn-primary'>Register</button>
             </form>
+            {
+                registerSuccess && 
+                <div className="mt-4 max-w-sm mx-auto">
+                <div className=" alert alert-success rounded-md">{registerSuccess}</div>
+                </div>
+            }
+            {registerError && <div className="mt-4 max-w-sm mx-auto">
+                <div className=" alert alert-error rounded-md">{registerError}</div>
+            </div>}
         </div>
     );
 };
